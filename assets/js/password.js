f9867bf47a970c48c6be9a3f75408511a365e226
DoodleBob <riley.foust18@gmail.com>
2021-09-12 13:23:35 -0400

    added length and criteria alerts

---
+++ assets/js/password.js
@@ -0,0 +1,34 @@
+// Assignment code here
+var generatePassword = function() {
+  var passwordLength = prompt("How long would you like your password to be?");
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null) {
+    alert("Please choose a length between 8 and 128.");
+    return generatePassword();
+  }
+  localStorage.setItem("Password length", passwordLength);
+
+  var criteria = ["lowercase letters", "uppercase letters", "numbers", "special chracters"]
+
+  for(var i = 0; i < criteria.length; i++) {
+    var answer = confirm(`Would you like to include ${criteria[i]} in your password?`);
+    if (answer === true || answer === false) {
+      localStorage.setItem(criteria[i], answer);
+    }
+  };
+
+  
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
